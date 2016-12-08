06f802a962b62773a222be210fe0c2a738d006ed
Tedozi Manson <duyphan.developer@gmail.com>
2016-12-09 00:43:46 +0700

    Refactor

--- routes/web.php
+++ routes/web.php
@@ -6,55 +6,52 @@
  *
  */
 
-$router->group(['middleware' => 'web'], function (Router $router) {
+$adminRoute = config('webed.admin_route');
 
-    $adminRoute = config('webed.admin_route');
+$moduleRoute = 'acl';
 
-    $moduleRoute = 'acl';
+/**
+ * Admin routes
+ */
+$router->group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
+    $router->get('', function () {
+        return redirect()->to(route('admin::acl-roles.index.get'));
+    });
+    /**
+     * Roles
+     */
+    $router->get('/roles', 'RoleController@getIndex')
+        ->name('admin::acl-roles.index.get')
+        ->middleware('has-permission:view-roles');
+
+    $router->post('/roles', 'RoleController@postListing')
+        ->name('admin::acl-roles.index.get-json')
+        ->middleware('has-permission:view-roles');
+
+    $router->get('/roles/create', 'RoleController@getCreate')
+        ->name('admin::acl-roles.create.get')
+        ->middleware('has-permission:create-roles');
+
+    $router->get('/roles/edit/{id}', 'RoleController@getEdit')
+        ->name('admin::acl-roles.edit.get')
+        ->middleware('has-permission:edit-roles');
+
+    $router->post('/roles/edit/{id}', 'RoleController@postEdit')
+        ->name('admin::acl-roles.edit.post')
+        ->middleware('has-permission:edit-roles');
+
+    $router->delete('/roles/{id}', 'RoleController@deleteDelete')
+        ->name('admin::acl-roles.delete.delete')
+        ->middleware('has-permission:delete-roles');
 
     /**
-     * Admin routes
+     * Permissions
      */
-    $router->group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
-        $router->get('', function () {
-            return redirect()->to(route('admin::acl-roles.index.get'));
-        });
-        /**
-         * Roles
-         */
-        $router->get('/roles', 'RoleController@getIndex')
-            ->name('admin::acl-roles.index.get')
-            ->middleware('has-permission:view-roles');
+    $router->get('/permissions', 'PermissionController@getIndex')
+        ->name('admin::acl-permissions.index.get')
+        ->middleware('has-permission:view-permissions');
 
-        $router->post('/roles', 'RoleController@postListing')
-            ->name('admin::acl-roles.index.get-json')
-            ->middleware('has-permission:view-roles');
-
-        $router->get('/roles/create', 'RoleController@getCreate')
-            ->name('admin::acl-roles.create.get')
-            ->middleware('has-permission:create-roles');
-
-        $router->get('/roles/edit/{id}', 'RoleController@getEdit')
-            ->name('admin::acl-roles.edit.get')
-            ->middleware('has-permission:edit-roles');
-
-        $router->post('/roles/edit/{id}', 'RoleController@postEdit')
-            ->name('admin::acl-roles.edit.post')
-            ->middleware('has-permission:edit-roles');
-
-        $router->delete('/roles/{id}', 'RoleController@deleteDelete')
-            ->name('admin::acl-roles.delete.delete')
-            ->middleware('has-permission:delete-roles');
-
-        /**
-         * Permissions
-         */
-        $router->get('/permissions', 'PermissionController@getIndex')
-            ->name('admin::acl-permissions.index.get')
-            ->middleware('has-permission:view-permissions');
-
-        $router->post('/permissions', 'PermissionController@postListing')
-            ->name('admin::acl-permissions.index.post')
-            ->middleware('has-permission:view-permissions');
-    });
+    $router->post('/permissions', 'PermissionController@postListing')
+        ->name('admin::acl-permissions.index.post')
+        ->middleware('has-permission:view-permissions');
 });
